4e2b246695ae9cd69faf638c82562a3a36bc7091
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-06 13:08:12 +0530

    new contents added

---
+++ todo-app/models/Todo.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+export default Todo;
